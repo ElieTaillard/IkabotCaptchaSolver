083df8bb7a41594e265d14d9392807c93fd55a32
Xaalek <taillardelie@gmail.com>
2021-07-27 11:42:27 +0200

    Fix saved name : lower()

--- IkabotSimulation.py
+++ IkabotSimulation.py
@@ -16,7 +16,7 @@ def saveImg(textImg, captchaImg, response):
 
     array = chopImg(pil_image)
 
-    path = os.path.join(COLLECTION_FOLDER_PATH, name + ".png")
+    path = os.path.join(COLLECTION_FOLDER_PATH, name.lower() + ".png")
 
     img = array[int(response)-1]
     img.save(path)
