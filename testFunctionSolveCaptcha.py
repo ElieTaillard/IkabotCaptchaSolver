709ed325d818daf6cc51a6c61b2da53be9e3fc8c
Xaalek <taillardelie@gmail.com>
2021-07-27 11:45:15 +0200

    Add collection path in testFunctionSolveCaptcha

--- testFunctionSolveCaptcha.py
+++ testFunctionSolveCaptcha.py
@@ -4,12 +4,13 @@ from PIL import Image
 
 TEXT_IMAGE_PATH = "img/examples/txt1.png"
 CAPTCHA_IMAGE_PATH = "img/examples/img1.png"
+COLLECTION_FOLDER_PATH = "img/collection/"
 
 textImg = Image.open(TEXT_IMAGE_PATH)
 textImg.load()
 
 captchaImg = cv2.imread(CAPTCHA_IMAGE_PATH)
 
-result = solveCaptcha(textImg,captchaImg)
+result = solveCaptcha(textImg, captchaImg, COLLECTION_FOLDER_PATH)
 
 print(result)
